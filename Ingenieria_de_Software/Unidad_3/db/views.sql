a820cc0a88b5b87442a97a60de75a35d078d2d73
ItsMiich <l22140756@queretaro.tecnm.mx>
2026-05-11 01:18:36 -0600

    v1.0.2

--- Ingenieria_de_Software/Unidad_3/db/views.sql
+++ Ingenieria_de_Software/Unidad_3/db/views.sql
@@ -98,3 +98,141 @@ JOIN paciente p ON e.id_paciente = p.id_paciente
 JOIN usuario u ON e.id_medico = u.id_usuario
 JOIN diagnostico d ON e.id_diagnostico = d.id_diagnostico
 JOIN estatus_expediente ee ON e.id_estatus_expediente = ee.id_estatus_expediente;
+
+--VIEW paciente
+CREATE OR REPLACE VIEW digiclin.vw_paciente AS
+SELECT
+    p.id_paciente,
+    p.nombre_p,
+    p.apellido_pat,
+    p.apellido_mat,
+    CONCAT_WS(' ', p.nombre_p, p.apellido_pat, p.apellido_mat) AS nombre_completo,
+    p.fecha_nacimiento,
+    p.curp,
+    p.domicilio,
+    p.correo,
+    p.ocupacion,
+    p.telefono,
+    p.contacto_emergencia,
+    p.fecha_registro,
+
+    s.id_sexo,
+    s.nombre AS nombre_sexo,
+
+    ec.id_estado_civil,
+    ec.nombre AS nombre_estado_civil,
+
+    ts.id_tipo_sangre,
+    ts.tipo AS nombre_tipo_sangre,
+
+    ep.id_estatus_paciente,
+    ep.nombre_estatus
+
+FROM digiclin.paciente p
+INNER JOIN digiclin.sexo s
+    ON p.id_sexo = s.id_sexo
+LEFT JOIN digiclin.estado_civil ec
+    ON p.id_estado_civil = ec.id_estado_civil
+LEFT JOIN digiclin.tipo_sangre ts
+    ON p.id_tipo_sangre = ts.id_tipo_sangre
+INNER JOIN digiclin.estatus_paciente ep
+    ON p.id_estatus_paciente = ep.id_estatus_paciente;
+
+--VIEW paciente(medico/director)
+CREATE OR REPLACE VIEW digiclin.vw_paciente_completo AS
+SELECT
+    p.id_paciente,
+    p.nombre_p,
+    p.apellido_pat,
+    p.apellido_mat,
+    CONCAT_WS(' ', p.nombre_p, p.apellido_pat, p.apellido_mat) AS nombre_completo,
+
+    p.fecha_nacimiento,
+    EXTRACT(YEAR FROM AGE(CURRENT_DATE, p.fecha_nacimiento))::INTEGER AS edad,
+
+    s.id_sexo,
+    s.nombre AS nombre_sexo,
+
+    p.curp,
+    p.domicilio,
+
+    ec.id_estado_civil,
+    ec.nombre AS nombre_estado_civil,
+
+    p.correo,
+    p.ocupacion,
+    p.telefono,
+    p.contacto_emergencia,
+
+    ts.id_tipo_sangre,
+    ts.tipo AS nombre_tipo_sangre,
+
+    ep.id_estatus_paciente,
+    ep.nombre_estatus,
+
+    p.fecha_registro
+FROM digiclin.paciente p
+INNER JOIN digiclin.sexo s
+    ON p.id_sexo = s.id_sexo
+LEFT JOIN digiclin.estado_civil ec
+    ON p.id_estado_civil = ec.id_estado_civil
+LEFT JOIN digiclin.tipo_sangre ts
+    ON p.id_tipo_sangre = ts.id_tipo_sangre
+INNER JOIN digiclin.estatus_paciente ep
+    ON p.id_estatus_paciente = ep.id_estatus_paciente;
+
+--VIEW paciente (enfermero)
+CREATE OR REPLACE VIEW digiclin.vw_paciente_enfermero AS
+SELECT
+    p.id_paciente,
+    p.nombre_p,
+    p.apellido_pat,
+    p.apellido_mat,
+    CONCAT_WS(' ', p.nombre_p, p.apellido_pat, p.apellido_mat) AS nombre_completo,
+
+    p.fecha_nacimiento,
+    EXTRACT(YEAR FROM AGE(CURRENT_DATE, p.fecha_nacimiento))::INTEGER AS edad,
+
+    s.id_sexo,
+    s.nombre AS nombre_sexo,
+
+    p.curp,
+    p.telefono,
+    p.contacto_emergencia,
+
+    ts.id_tipo_sangre,
+    ts.tipo AS nombre_tipo_sangre,
+
+    ep.id_estatus_paciente,
+    ep.nombre_estatus
+FROM digiclin.paciente p
+INNER JOIN digiclin.sexo s
+    ON p.id_sexo = s.id_sexo
+LEFT JOIN digiclin.tipo_sangre ts
+    ON p.id_tipo_sangre = ts.id_tipo_sangre
+INNER JOIN digiclin.estatus_paciente ep
+    ON p.id_estatus_paciente = ep.id_estatus_paciente;
+
+--VIEW paciente (administrativo)
+CREATE OR REPLACE VIEW digiclin.vw_paciente_administrativo AS
+SELECT
+    p.id_paciente,
+    p.nombre_p,
+    p.apellido_pat,
+    p.apellido_mat,
+    CONCAT_WS(' ', p.nombre_p, p.apellido_pat, p.apellido_mat) AS nombre_completo,
+
+    p.curp,
+    p.fecha_nacimiento,
+    EXTRACT(YEAR FROM AGE(CURRENT_DATE, p.fecha_nacimiento))::INTEGER AS edad,
+
+    p.telefono,
+    p.correo,
+
+    ep.id_estatus_paciente,
+    ep.nombre_estatus,
+
+    p.fecha_registro
+FROM digiclin.paciente p
+INNER JOIN digiclin.estatus_paciente ep
+    ON p.id_estatus_paciente = ep.id_estatus_paciente;
